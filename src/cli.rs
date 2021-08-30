f277a517301fa54098c9fb146fcd5721a8f45bcf
shinespark <shnsprk@gmail.com>
2021-08-30 09:42:29 +0900

    Channel一覧を取得するメソッドを追加

--- src/cli.rs
+++ src/cli.rs
@@ -3,10 +3,17 @@ use crate::slack::SlackRequest;
 pub async fn statistics(token: &str) -> Result<(), Box<dyn std::error::Error>> {
     let slack_request = SlackRequest::new(token);
 
-    let emojis = slack_request.list_emoji().await?;
+    let emojis = slack_request.emoji_list().await?;
 
-    println!("{:?}", emojis);
     println!("{:?}", emojis.len());
+    println!("{:?}", emojis);
+
+    let channels = slack_request.channel_list().await?;
+
+    // TODO: channelに絞り込む？
+    // TODO: channelにpost取りにいくメソッド生やす？
+    println!("{:?}", channels.len());
+    println!("{:?}", channels);
 
     Ok(())
 }
--- src/slack.rs
+++ src/slack.rs
@@ -2,6 +2,8 @@ use reqwest::{header, Client};
 use serde::Deserialize;
 use std::collections::HashMap;
 
+const CONTENT_TYPE: &str = "application/json; charset=utf-8";
+const CONVERSATIONS_LIST_API: &str = "https://slack.com/api/conversations.list";
 const EMOJI_LIST_API: &str = "https://slack.com/api/emoji.list";
 
 pub struct SlackRequest {
@@ -9,7 +11,36 @@ pub struct SlackRequest {
 }
 
 #[derive(Debug, Deserialize)]
-struct ListEmojiResponse {
+struct ConversationsListResponse {
+    ok: bool,
+    channels: Vec<Channel>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Channel {
+    id: String,
+    name: String,
+    is_channel: bool,
+    is_group: bool,
+    is_im: bool,
+    created: u64,
+    creator: String,
+    is_archived: bool,
+    is_general: bool,
+    unlinked: u64,
+    name_normalized: String,
+    is_shared: bool,
+    is_ext_shared: bool,
+    is_org_shared: bool,
+    is_pending_ext_shared: bool,
+    is_member: bool,
+    is_private: bool,
+    is_mpim: bool,
+    num_members: u64,
+}
+
+#[derive(Debug, Deserialize)]
+struct EmojiListResponse {
     ok: bool,
     emoji: HashMap<String, String>,
 }
@@ -27,16 +58,30 @@ impl SlackRequest {
         }
     }
 
-    pub async fn list_emoji(&self) -> Result<Vec<Emoji>, Box<dyn std::error::Error>> {
+    pub async fn channel_list(&self) -> Result<Vec<Channel>, Box<dyn std::error::Error>> {
+        let client = Client::builder().build()?;
+        let res = client
+            .get(CONVERSATIONS_LIST_API)
+            .header(header::AUTHORIZATION, &self.token)
+            .header(header::CONTENT_TYPE, CONTENT_TYPE)
+            .send()
+            .await?;
+        let body = res.text().await?;
+        let json: ConversationsListResponse = serde_json::from_str(&body)?;
+
+        Ok(json.channels)
+    }
+
+    pub async fn emoji_list(&self) -> Result<Vec<Emoji>, Box<dyn std::error::Error>> {
         let client = Client::builder().build()?;
         let res = client
             .get(EMOJI_LIST_API)
             .header(header::AUTHORIZATION, &self.token)
-            .header(header::CONTENT_TYPE, "application/json; charset=utf-8")
+            .header(header::CONTENT_TYPE, CONTENT_TYPE)
             .send()
             .await?;
         let body = res.text().await?;
-        let json: ListEmojiResponse = serde_json::from_str(&body)?;
+        let json: EmojiListResponse = serde_json::from_str(&body)?;
 
         let emojis = json
             .emoji
